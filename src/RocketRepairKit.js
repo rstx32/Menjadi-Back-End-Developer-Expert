9f77c15677d63e036bd1d39ecac4b288420dc24e
rstx32 <rstx32@gmail.com>
2023-09-05 10:06:49 +0700

    test double

---
+++ src/RocketRepairKit.js
@@ -0,0 +1,19 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable arrow-body-style */
+class RocketRepairKit {
+  constructor(objA, objB, objC) {
+    this.objA = objA
+    this.objB = objB
+    this.objC = objC
+  }
+
+  repair(rocket) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        return resolve(`${rocket.name} repaired!`)
+      }, 500)
+    })
+  }
+}
+
+module.exports = RocketRepairKit
