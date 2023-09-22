b5c6bd5ccfd2ca65115d77363a39ffcb35b66724
rstx <rstx32@gmail.com>
2023-09-22 11:45:24 +0700

    add BcryptPasswordHash

--- src/Infrastructures/repository/_test/UserRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/UserRepositoryPostgres.test.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-useless-path-segments */
-const UsersTableTestHelper = require('../../../../../tests/UsersTableTestHelper')
-const InvariantError = require('../../../../Commons/exceptions/InvariantError')
-const RegisterUser = require('../../../../Domains/users/entities/RegisterUser')
-const RegisteredUser = require('../../../../Domains/users/entities/RegisteredUser')
-const pool = require('../../../database/postgres/pool')
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper')
+const InvariantError = require('../../../Commons/exceptions/InvariantError')
+const RegisterUser = require('../../../Domains/users/entities/RegisterUser')
+const RegisteredUser = require('../../../Domains/users/entities/RegisteredUser')
+const pool = require('../../database/postgres/pool')
 const UserRepositoryPostgres = require('../UserRepositoryPostgres')
 
 describe('UserRepositoryPostgres', () => {
